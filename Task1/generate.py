d9eb8e17d5d33b61c649e0fd24dbd03a0f1bdb38
aashima2310 <aashimasingh2310@gmail.com>
2026-04-04 03:36:01 +0530

    Update generate.py

--- Task1/generate.py
+++ Task1/generate.py
@@ -5,16 +5,16 @@ from src.model import GPT
 import config
 
 def generate(model, tokenizer, seed_text, max_tokens=500):
-    encoded = tokenizer.encode(seed_text)
-    x = torch.tensor(encoded).unsqueeze(0).to(config.device)
+    encoded = tokenizer.encode(seed_text).ids
+    x       = torch.tensor(encoded).unsqueeze(0).to(config.DEVICE)
 
     for _ in range(max_tokens):
-        x_crop = x[:, -config.block_size:]
-        logits  = model(x_crop)
-        logits  = logits[:, -1, :]
-        probs   = F.softmax(logits, dim=-1)
+        x_crop     = x[:, -config.BLOCK_SIZE:]
+        logits     = model(x_crop)
+        logits     = logits[:, -1, :]
+        probs      = F.softmax(logits, dim=-1)
         next_token = torch.multinomial(probs, num_samples=1)
-        x = torch.cat([x, next_token], dim=1)
+        x          = torch.cat([x, next_token], dim=1)
 
     decoded = tokenizer.decode(x[0].tolist())
     return decoded
@@ -24,14 +24,15 @@ if __name__ == "__main__":
     encoded, tokenizer, vocab_size = load_data('data/input.txt')
 
     model = GPT(
-        vocab_size = config.vocab_size,
-        d_model    = config.d_model,
-        block_size = config.block_size,
-        n_heads    = config.n_heads,
-        n_layers   = config.n_layers
-    ).to(config.device)
+        vocab_size = config.VOCAB_SIZE,
+        d_model    = config.D_MODEL,
+        block_size = config.BLOCK_SIZE,
+        n_heads    = config.N_HEADS,
+        n_layers   = config.N_LAYERS
+    ).to(config.DEVICE)
 
-    model.load_state_dict(torch.load('best_model.pt', map_location=config.device))
+    model.load_state_dict(torch.load('best_model.pt',
+                          map_location=config.DEVICE))
     model.eval()
 
     output = generate(model, tokenizer, "To be", max_tokens=500)
