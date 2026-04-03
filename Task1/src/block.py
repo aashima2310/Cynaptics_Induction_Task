6a2e3f4b6fec8cd621fc02c7e16c49bf1aa321af
aashima2310 <aashimasingh2310@gmail.com>
2026-04-04 03:33:50 +0530

    Update block.py

--- Task1/src/block.py
+++ Task1/src/block.py
@@ -3,15 +3,15 @@ from src.attention import MultiHeadAttention
 from src.ffn import FeedForward
 
 class TransformerBlock(nn.Module):
-    def __init__(self,d_model,n_heads, block_size):
+    def __init__(self, d_model, n_heads, block_size):
         super().__init__()
         self.attention = MultiHeadAttention(d_model, n_heads, block_size)
-        self.ffn = FeedForward(d_model)
-        self.ln1 = nn.LayerNorm(d_model)
-        self.ln2 = nn.LayerNorm(d_model)
-        self.dropout = nn.Dropout(0.2)
-    
+        self.ffn       = FeedForward(d_model)
+        self.ln1       = nn.LayerNorm(d_model)
+        self.ln2       = nn.LayerNorm(d_model)
+        self.dropout   = nn.Dropout(0.2)
+
     def forward(self, x):
-        x = x + self.dropout(self.attention(self.ln1(x)))   # attention
-        x = x + self.dropout(self.ffn(self.ln2(x)))      # feedforward
+        x = x + self.dropout(self.attention(self.ln1(x)))
+        x = x + self.dropout(self.ffn(self.ln2(x)))
         return x
