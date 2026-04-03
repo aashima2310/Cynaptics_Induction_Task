dcc46f9fd5a3881ce11dc17dd0f372f2210a9ad3
aashima2310 <aashimasingh2310@gmail.com>
2026-04-04 02:59:24 +0530

    Update ffn.py

--- Task1/src/ffn.py
+++ Task1/src/ffn.py
@@ -1,11 +1,12 @@
 import torch.nn as nn
 
 class FeedForward(nn.Module):
-    def __init__(self, d_model):
-        super().__init__()          
+    def __init__(self, d_model, dropout=0.2):
+        super().__init__()
         self.net = nn.Sequential(
             nn.Linear(d_model, 4 * d_model),
             nn.GELU(),
+            nn.Dropout(dropout),
             nn.Linear(4 * d_model, d_model)
         )
 
