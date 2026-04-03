fcf442b5ed8efafb67cc88ad177842a5f891dae0
aashima2310 <aashimasingh2310@gmail.com>
2026-04-04 03:30:15 +0530

    Update tokenizer.py

--- Task1/src/tokenizer.py
+++ Task1/src/tokenizer.py
@@ -1,15 +1,17 @@
 from tokenizers import ByteLevelBPETokenizer
+import os
 
 def train_tokenizer(file_path, vocab_size=1000):
     tokenizer = ByteLevelBPETokenizer()
     tokenizer.train(
-        files      = [file_path],
-        vocab_size = vocab_size,
+        files         = [file_path],
+        vocab_size    = vocab_size,
         min_frequency = 2
     )
     tokenizer.save_model('.', 'shakespeare')
     print(f"Tokenizer trained! vocab size = {vocab_size}")
     return tokenizer
+
 
 def load_tokenizer():
     tokenizer = ByteLevelBPETokenizer(
@@ -17,6 +19,3 @@ def load_tokenizer():
         'shakespeare-merges.txt'
     )
     return tokenizer
-
-if __name__ == "__main__":
-    train_tokenizer('data/input.txt', vocab_size=1000)
